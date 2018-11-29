fcd46f2babada8ee61b1c9d63a96d86c757961a0
Brunhild <nechwi1121@gmail.com>
2018-11-29 22:14:53 +0900

    프로젝트 위치 정리

---
+++ Assets/Scripts/TestScript/CSceneSwitch.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSceneSwitch : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
