867422d569f88ae035deecff71f68223c6c8c795
Brunhild <nechwi1121@gmail.com>
2018-11-29 22:01:11 +0900

    데이터베이스 작동 테스트

--- Assets/Scripts/TestScript/ScriptableObjectTest.cs
+++ Assets/Scripts/TestScript/ScriptableObjectTest.cs
@@ -1,24 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScriptableObjectTest : MonoBehaviour
 {
+    [SerializeField]
+    private Text _dataName = null;
+
+    [SerializeField]
+    private Text _dataDesc = null;
+
+    private CItemDatabase _itemDb = null;
+    private int _dbIndex = 0;
 
     // Use this for initialization
     void Start()
     {
-        CItemDatabase itemDb = Resources.Load<CItemDatabase>("Database/ItemDatabase");
+        _itemDb = Resources.Load<CItemDatabase>("Database/ItemDatabase");
 
-        foreach (CItem item in itemDb.items)
+        foreach (CItem item in _itemDb.items)
         {
             Debug.Log(item.title);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnNextClick()
     {
+        if (_itemDb != null)
+        {
+            ++_dbIndex;
+            if (_dbIndex >= _itemDb.items.Count)
+            {
+                _dbIndex = 0;
+            }
 
+            _dataName.text = _itemDb.items[_dbIndex].title;
+            _dataDesc.text = _itemDb.items[_dbIndex].description;
+        }
     }
+
+    public void OnPrevClick()
+    {
+        if (_itemDb != null)
+        {
+            --_dbIndex;
+            if (_dbIndex < 0)
+            {
+                _dbIndex = _itemDb.items.Count - 1;
+            }
+
+            _dataName.text = _itemDb.items[_dbIndex].title;
+            _dataDesc.text = _itemDb.items[_dbIndex].description;
+        }
+    }
+
 }
