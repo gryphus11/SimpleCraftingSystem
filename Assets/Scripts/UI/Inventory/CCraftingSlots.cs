85364c9dd9e5b8ed62d3df23c3cde3331cf4880a
Brunhild <nechwi1121@gmail.com>
2018-11-30 19:19:50 +0900

    조합시스템 구축 (누락된 파일 업로드)

---
+++ Assets/Scripts/UI/Inventory/CCraftingSlots.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCraftingSlots : MonoBehaviour
+{
+    [SerializeField]
+    private CCraftRecipeDatabase _recipeDatabase = null;
+
+    private List<CUIItem> _uiItems = new List<CUIItem>();
+
+    [SerializeField]
+    private CUIItem _craftResultSlot = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        _uiItems = GetComponent<CSlotPanel>().uiItems;
+        _uiItems.ForEach(item => item.isCraftingSlot = true);
+
+        if (_recipeDatabase == null)
+        {
+            _recipeDatabase = Resources.Load<CCraftRecipeDatabase>(CUtillity.databaseResourcesPath + "RecipeDatabase");
+        }
+    }
+
+    public void UpdateRecipe()
+    {
+        Debug.Log("####CCraftingSlots::UpdateRecipe");
+        int[] itemTable = new int[_uiItems.Count];
+
+        for (int i = 0; i < _uiItems.Count; ++i)
+        {
+            if (_uiItems[i].item != null)
+            {
+                itemTable[i] = _uiItems[i].item.id;
+            }
+        }
+
+        CItem itemToCraft = _recipeDatabase.CheckRecipe(itemTable);
+        UpdateCraftingResultSlot(itemToCraft);
+    }
+
+    private void UpdateCraftingResultSlot(CItem itemToCraft)
+    {
+        _craftResultSlot.UpdateItem(itemToCraft);
+    }
+}
---
+++ Assets/Scripts/UI/Inventory/CUICraftResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CUICraftResult : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    private CSlotPanel _slotPanel = null;
+
+    [SerializeField]
+    private CInventory _inventory = null;
+
+    /// <summary>
+    /// 조합완료 칸을 누른 경우
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _slotPanel.EmptyAllSlots();
+        _inventory.playerItems.Add(GetComponent<CUIItem>().item);
+    }
+}
